3d34aeb91a8fcedcdf2c69678bd086ff390f3ad7
Joachim Gleissner <jgleissner@suse.com>
2021-10-14 13:43:06 +0100

    Drop all SLE12 SP3 related data

--- data/overlayfiles/kiwi-hook-handle-machine-id/kiwi-hooks/handleMachineID.sh
+++
@@ -1,7 +0,0 @@
-# remove any existing machine IDs
-rm -f /etc/machine-id
-rm -f /var/lib/dbus/machine-id
-# generate DBUS machine ID
-dbus-uuidgen --ensure
-# if on systemd, run systemd-machine-id-setup to init /etc/machine_id from dbus
-test -x /usr/bin/systemd-machine-id-setup && /usr/bin/systemd-machine-id-setup
