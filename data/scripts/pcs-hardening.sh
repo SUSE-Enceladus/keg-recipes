392832f1ce92105905261e1ff95d880e1237c8ba
sampsone <esampson@suse.com>
2023-07-26 17:33:41 -0500

    Use oscap with pcs-hardening not remediation script and turn off bin/fail
    The remediation script:
    /usr/share/scap-security-guide/bash/sle15-script-pcs-hardening.sh
    
    blindly applies remediation even if it is not needed
    
    using:
    oscap xccdf eval --remediate --profile pcs-hardening /usr/share/xml/scap/ssg/content/ssg-sle15-ds.xml
    
    is better, since it will only attempt to apply remediation when needed.
    
    Do not execute /bin/fail if the oscap command returns an error.
    This allows builds to complete even if remediation returns
    and error

--- data/scripts/pcs-hardening.sh
+++ data/scripts/pcs-hardening.sh
@@ -1,2 +1,5 @@
 # run image hardening script
-/usr/bin/bash -e /usr/share/scap-security-guide/bash/sle15-script-pcs-hardening.sh
+echo "run oscap -profile pcs-hardening"
+oscap xccdf eval --remediate --profile pcs-hardening /usr/share/xml/scap/ssg/content/ssg-sle15-ds.xml || {
+    echo "!!!FAILED: --profile pcs-hardening"
+}
