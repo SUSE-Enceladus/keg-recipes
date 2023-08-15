0ff965ae6745457f2b2a91802f46d37da1f69b92
Joachim Gleissner <jgleissner@suse.com>
2023-08-15 16:26:58 +0100

    PCS hardening: disable any sysctl rule
    Disable any sysctl rules as they do not work properly in a chroot env.

--- data/scripts/pcs-hardening-workarounds.sh
+++ data/scripts/pcs-hardening-workarounds.sh
@@ -2,10 +2,12 @@
 # them until there is an upstream solution.
 # NOTE: rule pam_disable_automatic_configuration uses a bash input redirection
 # type that required /proc which is not availble in kiwi's create step.
+# NOTE: disable any potential sysctl rules, they do not work properly in chroot.
 rules_to_disable="\
 xccdf_org.ssgproject.content_rule_ensure_logrotate_activated
 xccdf_org.ssgproject.content_rule_service_firewalld_enabled
-xccdf_org.ssgproject.content_rule_pam_disable_automatic_configuration"
+xccdf_org.ssgproject.content_rule_pam_disable_automatic_configuration
+xccdf_org.ssgproject.content_rule_.*sysctl"
 ssg_file="/usr/share/xml/scap/ssg/content/ssg-sle15-ds.xml"
 
 for rule in $rules_to_disable ; do
