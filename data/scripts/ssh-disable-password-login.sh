c72ad6c0e0b1b6a853652bab87c669badab52571
Joachim Gleissner <jgleissner@suse.com>
2024-02-06 16:13:29 +0000

    Add SLE Micro 6.0 data and image definitions

--- data/scripts/ssh-disable-password-login.sh
+++ data/scripts/ssh-disable-password-login.sh
@@ -1,3 +1,4 @@
 # Disable password based login via ssh
+test -f /etc/ssh/sshd_config || cp /usr/etc/ssh/sshd_config /etc/ssh/
 sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/' \
     /etc/ssh/sshd_config
