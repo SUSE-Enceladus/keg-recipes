6673e01e1946e3be5936035ac1526ec9d6b79aa5
Joachim Gleissner <jgleissner@suse.com>
2025-08-13 14:11:18 +0100

    Drop work-around for waagent auto-update config
    Drop script for disabling auto-update in /etc/waagent.conf as a fixed package
    was released, rendering it obsolete.

--- data/scripts/waagent-disable-auto-update.sh
+++
@@ -1,5 +0,0 @@
-# Disable agent auto-update
-sed -i -e 's/AutoUpdate.Enabled=y/AutoUpdate.Enabled=n/' \
-    /etc/waagent.conf
-sed -i -r -e 's/(#\s*)?AutoUpdate.UpdateToLatestVersion=y/AutoUpdate.UpdateToLatestVersion=n/' \
-    /etc/waagent.conf
