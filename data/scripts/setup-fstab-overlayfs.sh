41eca6f3de713cb96a9fb4ca800cee4a182f46be
Joachim Gleissner <jgleissner@suse.com>
2026-01-22 17:22:35 +0000

    Fix quotation issue in script

--- data/scripts/setup-fstab-overlayfs.sh
+++ data/scripts/setup-fstab-overlayfs.sh
@@ -7,9 +7,9 @@ data="\
 set -eux
 /usr/sbin/setup-fstab-for-overlayfs
 # If /var is on a different partition than /...
-if [ \"$(findmnt -snT / -o SOURCE)\" != \"$(findmnt -snT /var -o SOURCE)\" ]; then
+if [ \"\$(findmnt -snT / -o SOURCE)\" != \"\$(findmnt -snT /var -o SOURCE)\" ]; then
   # ... set options for autoexpanding /var
-  gawk -i inplace '$2 == \"/var\" { $4 = $4\",x-growpart.grow,x-systemd.growfs\" } { print $0 }' /etc/fstab
+  gawk -i inplace '\$2 == \"/var\" { \$4 = \$4\",x-growpart.grow,x-systemd.growfs\" } { print \$0 }' /etc/fstab
 fi"
 ind=$(echo "$data" | sed -r -e 's/(\s*)(.*)/\1/ ; q')
 echo "$data" | sed -r -e "s/^$ind//" > /etc/fstab.script
