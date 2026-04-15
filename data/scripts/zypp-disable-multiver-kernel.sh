ece74277d8e8f217deebd1e66330684937c99e9d
Joachim Gleissner <jgleissner@suse.com>
2026-04-15 17:26:39 +0100

    Handle zypp configuration directory dynamically
    Install zypper configurations into /etc/zypp/zypp.conf.d if there is no
    /etc/zypper/zypp.conf.
    Only disable delta RPMs for SLE 15 targets (obsolete from SLE 15 SP7).

--- data/scripts/zypp-disable-multiver-kernel.sh
+++ data/scripts/zypp-disable-multiver-kernel.sh
@@ -1,1 +1,5 @@
-sed -i -e 's/latest,latest-1,running/latest,running/' /etc/zypp/zypp.conf
+if [ -f /etc/zypp/zypp.conf]; then
+    sed -i -e 's/latest,latest-1,running/latest,running/' /etc/zypp/zypp.conf
+else
+    echo "multiversion.kernels = latest" > /etc/zypp/zypp.conf.d/40_multiver_kernel_latest.conf
+fi
