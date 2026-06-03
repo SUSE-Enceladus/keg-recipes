f0679e1bef11babb6d57be58a895da8c0e09a9d2
Joachim Gleissner <jgleissner@suse.com>
2026-06-03 13:07:32 +0100

    Drop obsolete work-around for non-FIPS algorithm in ssh config

--- data/scripts/ssh-drop-non-fips-kex-algo.sh
+++
@@ -1,4 +0,0 @@
-# Drop Key Exchange Algorithm that is not fips approved for ssh
-# bsc#1259495, bsc#1262555
-sed -i -e 's/mlkem768x25519-sha256,*//g; s/,,/,/g; s/,$//g' \
-    /etc/crypto-policies/back-ends/opensshserver.config
