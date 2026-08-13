187e7983499ef8bc60138cd659380a9d5af93c49
Joachim Gleissner <jgleissner@suse.com>
2026-08-13 12:58:06 +0100

    Fix CHOST FIPS mode setup
    Move some configuration bits from images.sh to config.sh.
    Install package crypto-policies-scripts.
    Use update-crypto-policies script to enable FIPS.

--- data/scripts/enable-fips-mode.sh
+++ data/scripts/enable-fips-mode.sh
@@ -1,3 +1,1 @@
-cp /usr/share/crypto-policies/policies/FIPS.pol /etc/crypto-policies/state/CURRENT.pol
-echo FIPS > /etc/crypto-policies/state/current
-echo FIPS > /etc/crypto-policies/config
+update-crypto-policies --no-reload --set FIPS
