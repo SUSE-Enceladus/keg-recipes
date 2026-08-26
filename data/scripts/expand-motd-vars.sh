c883e82d2b6c77b4bdae2a7f17842a5a61c509ee
Joachim Gleissner <jgleissner@suse.com>
2026-08-26 09:16:15 +0100

    Fix some issues with motd
    - Provide product ID and version as variables for motd snippets
    - Replace product specific documentation links motd snippets with generic one
    - Include documentation links in all products
    - Include software management hint in SLE HPC PAYG motd
    - Include registration motd hint in SLE HPC BYOS motd
    - Install SLE_HPC-release instead of sles-release in SLE HPC recipes

--- data/scripts/expand-motd-vars.sh
+++ data/scripts/expand-motd-vars.sh
@@ -4,11 +4,16 @@ source /etc/os-release
 
 OS_PRETTY_NAME="$PRETTY_NAME"
 OS_VERSION_MAJOR="${VERSION_ID%.*}"
-OS_VERSION="${VERSION_ID}"
+OS_VERSION="${VERSION}"
 ARCH="`uname -m`"
+PROD_ID="$(echo $CPE_NAME | cut -d: -f 4 | tr '_' '-')"
+PROD_VERSION="${VERSION}"
 
-test -f etc/products.d/SLES_SAP.prod && \
+# SLES for SAP has standard SLES /etc/os-release
+if [ -f etc/products.d/SLES_SAP.prod ]; then
     OS_PRETTY_NAME="$(echo $OS_PRETTY_NAME | sed -e 's/Server/Server for SAP Applications/')"
+    PROD_ID="sles-sap"
+fi
 
 # get extension info (if installed)
 for prod in /etc/products.d/*prod ; do
@@ -18,12 +23,19 @@ for prod in /etc/products.d/*prod ; do
     EXTENSION_NAME=${EXTENSION_NAME/ Extension/}
 done
 
+if [ -n "${EXTENSION_NAME}" -a "${EXTENSION_NAME/Multi-Linux Manager/}" != "${EXTENSION_NAME}" ]; then
+    PROD_ID="multi-linux-manager"
+    PROD_VERSION="$EXTENSION_VERSION"
+fi
+
 motd_func="\
 s/{OS_PRETTY_NAME}/$OS_PRETTY_NAME/g
 s/{OS_VERSION_MAJOR}/$OS_VERSION_MAJOR/g
 s/{OS_VERSION}/$OS_VERSION/g
 s/{EXTENSION_NAME}/$EXTENSION_NAME/g
 s/{EXTENSION_VERSION}/$EXTENSION_VERSION/g
+s/{PROD_ID}/$PROD_ID/g
+s/{PROD_VERSION}/$PROD_VERSION/g
 s/{ARCH}/$ARCH/g"
 
 for motd in /usr/lib/motd.d/* ; do
